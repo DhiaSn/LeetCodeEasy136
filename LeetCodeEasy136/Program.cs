cddc01284097f6827cbbf88501e3818452d45b2d
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-08-03 19:29:36 +0100

    Add project files.

---
+++ LeetCodeEasy136/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeEasy136
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result1 = Solution.SingleNumber([2, 2, 1]);
+            if (result1 != 1)
+            {
+                Console.WriteLine($"result1 is: {result1} instead of 1");
+                return;
+            }
+            int result2 = Solution.SingleNumber([4, 1, 2, 1, 2]);
+            if (result2 != 4)
+            {
+                Console.WriteLine($"result2 is: {result2} instead of 4");
+                return;
+            }
+
+            int result3 = Solution.SingleNumber([1]);
+            if (result3 != 1)
+            {
+                Console.WriteLine($"result3 is: {result3} instead of 1");
+                return;
+            }
+
+            Console.WriteLine($"it works perfectly!");
+        }
+    }
+}
---
+++ LeetCodeEasy136/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeEasy136
+{
+    public static class Solution
+    {
+        public static int SingleNumber(int[] nums)
+        {
+            if(nums.Length == 1)
+                return nums[0];
+
+            Dictionary<int,int> track = new Dictionary<int,int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (track.TryGetValue(nums[i], out int value))
+                    track[nums[i]] = ++value;
+                else
+                    track.Add(nums[i], 1);
+            }
+
+            return track.FirstOrDefault(x => x.Value == 1).Key; 
+        }
+    }
+}
